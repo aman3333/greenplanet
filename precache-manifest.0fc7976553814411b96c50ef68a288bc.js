bf291d2614ea406fed80d1719ed5a4f9afb3967c
Aman Jain <amanjain.3331@gmail.com>
2019-09-25 10:52:10 +0530

    added transaction

---
+++ precache-manifest.0fc7976553814411b96c50ef68a288bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8b4a756b439c4a7c5bad",
+    "url": "/static/js/main.8b4a756b.chunk.js"
+  },
+  {
+    "revision": "f55b9329199e319acc89",
+    "url": "/static/js/2.f55b9329.chunk.js"
+  },
+  {
+    "revision": "8b4a756b439c4a7c5bad",
+    "url": "/static/css/main.8dcb76ba.chunk.css"
+  },
+  {
+    "revision": "f55b9329199e319acc89",
+    "url": "/static/css/2.4f096862.chunk.css"
+  },
+  {
+    "revision": "46474d3b597b9889b95e9ad1f70ed427",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d10148959796a70afee5a9f92444096.js"
+  "/precache-manifest.0fc7976553814411b96c50ef68a288bc.js"
 );
 
 workbox.clientsClaim();
