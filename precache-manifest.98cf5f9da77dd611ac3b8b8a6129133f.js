01f0b50cbd74e91a5894f2a0f3f96f3e770489b1
Aman Jain <amanjain.3331@gmail.com>
2019-11-08 17:42:31 +0530

    new

---
+++ precache-manifest.98cf5f9da77dd611ac3b8b8a6129133f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "98ff963740b7939eaba6",
+    "url": "/static/js/main.98ff9637.chunk.js"
+  },
+  {
+    "revision": "7ad532155dbf56bc69f7",
+    "url": "/static/js/2.7ad53215.chunk.js"
+  },
+  {
+    "revision": "98ff963740b7939eaba6",
+    "url": "/static/css/main.6a574472.chunk.css"
+  },
+  {
+    "revision": "7ad532155dbf56bc69f7",
+    "url": "/static/css/2.4f096862.chunk.css"
+  },
+  {
+    "revision": "caca7a1ba7acf449df3e83fac59e29b2",
+    "url": "/index.html"
+  }
+];
